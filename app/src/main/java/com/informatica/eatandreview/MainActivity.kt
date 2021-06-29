726870ba73f2d3c1ec83fd712d5c8e507cb34eaf
PedruxMendoza <pedrux_mendoza@hotmail.com>
2021-06-29 17:00:23 -0600

    Primer Comit

---
+++ app/src/main/java/com/informatica/eatandreview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.informatica.eatandreview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
